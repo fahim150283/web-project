25f2cb3f7466279d1b63018c708e57766b349ed8
Abul Hossain Chowdhury <fahim150283@gmail.com>
2022-06-08 16:57:59 +0600

    jsfunctions

---
+++ jsfunction.js
@@ -0,0 +1,16 @@
+function iseven(num){
+    if(num%2==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function factorial(num){
+    var x=1;
+    for(let i=num; i>0; i--){
+        x=x*i;
+    }
+    return x;
+}
